6e5f506dbeb403ffe6a97adacf7fad9db30bca63
Liam Wears <liamwears@fastmail.com>
2024-07-08 08:37:46 +0200

    refactor(fe components): simplified html

--- frontend/src/components/SynonymWithWords.tsx
+++ frontend/src/components/SynonymWithWords.tsx
@@ -11,20 +11,16 @@ const SynonymWithWords: React.FC<WordWithSynonymsProps> = ({
 }) => {
   return (
     <div className="text-center flex gap-4 flex-col">
-      <h1 className="text-4xl">
-        Synonym: <span className="text-primary">{synonym}</span>
-      </h1>
-      <div className="flex justify-center gap-2">
-        <p>{'['}</p>
+      <h1 className="text-4xl text-primary">{synonym}</h1>
+      <div className="flex justify-center">
+        <p className="text-white">{'['}&nbsp;</p>
         {words.map((word, index) => (
-          <React.Fragment key={index}>
-            {index > 0 && ', '}
-            <span key={index} className="text-md text-green-300">
-              {word}
-            </span>
-          </React.Fragment>
+          <span key={index} className="text-md text-green-300">
+            {index > 0 && <span className="text-white">,&nbsp;</span>}
+            {word}
+          </span>
         ))}
-        <p>{']'}</p>
+        <p className="text-white">&nbsp;{']'}</p>
       </div>
     </div>
   );
--- frontend/src/components/WordWithSynonyms.tsx
+++ frontend/src/components/WordWithSynonyms.tsx
@@ -11,20 +11,16 @@ const WordWithSynonyms: React.FC<WordWithSynonymsProps> = ({
 }) => {
   return (
     <div className="text-center flex gap-4 flex-col">
-      <h1 className="text-4xl">
-        Word: <span className="text-primary">{word}</span>
-      </h1>
-      <div className="flex justify-center gap-2 flex-wrap">
-        <p>{'['}</p>
+      <h1 className="text-4xl text-primary">{word}</h1>
+      <div className="flex justify-center flex-wrap">
+        <p className="text-white">{'['}&nbsp;</p>
         {synonyms.map((synonym, index) => (
-          <React.Fragment key={index}>
-            {index > 0 && ','}
-            <span key={index} className="text-md text-green-300">
-              {synonym}
-            </span>
-          </React.Fragment>
+          <span key={index} className="text-md text-green-300">
+            {index > 0 && <span className="text-white">,&nbsp;</span>}
+            {synonym}
+          </span>
         ))}
-        <p>{']'}</p>
+        <p className="text-white">&nbsp;{']'}</p>
       </div>
     </div>
   );
