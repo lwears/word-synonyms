98faacb0dc1721e7e0c81f933115db39efbfa5db
Liam Wears <liamwears@fastmail.com>
2024-07-28 11:12:07 +0200

    chore(update eslint): update eslint to 9 and flat config

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,49 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import tsParser from '@typescript-eslint/parser'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginQuery.configs['flat/recommended'],
+  eslintPluginUnicorn.configs['flat/recommended'],
+  eslintPluginPrettierRecommended,
+  {
+    ignores: [
+      '**/dist',
+      '**/eslintrc_old.cjs',
+      '**/postcss.config.js',
+      '**/vite-env.d.ts',
+    ],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+  },
+  {
+    languageOptions: { globals: globals.browser, parser: tsParser },
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      '@typescript-eslint/no-namespace': 'off',
+      'unicorn/filename-case': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/numeric-separators-style': 'off',
+      'unicorn/no-null': 'off',
+    },
+  },
+]
